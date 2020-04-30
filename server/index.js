025d749d89b30f14738be3c9df1a33e23aa84099
David McCormick <davidmccormick27410@gmail.com>
2020-04-30 11:19:05 -0400

    Auth added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app = express();
+const passportConfig = require('./services/passport');
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there!' });
-});
-// nothing
-console.log('Running...');
+const app = express();
+require('./routes/authRoutes')(app);
+
+
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
